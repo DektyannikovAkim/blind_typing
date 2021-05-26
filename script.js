39d09b020dbd48e0fe5ca2269cc8990a8e98ec56
Akim <dektyannikov.akim@mail.ru>
2021-05-27 00:31:55 +0700

    Начал реализацию функционала проверки вводимых символов

--- script.js
+++ script.js
@@ -10,25 +10,35 @@ function get_text() {
         })
 }
 
-function render_text(num_of_par) {
+function render_text(numOfPar) {
     get_text().then(res => {
-        for (let i = 0; i < num_of_par; i++) {
+        for (let i = 0; i < numOfPar; i++) {
             let p = document.createElement('p');
             for (let x = 0; x < res[i].length; x++) {
-                p.insertAdjacentHTML('beforeend', `<span>${res[i][x]}</span>`)
+                p.insertAdjacentHTML('beforeend', `<span class="letter" data-num="${x}">${res[i][x]}</span>`)
             }
             textCont.append(p);
         }
     })
 }
 
+function startTyping() {
+
+}
+
 let textCont = document.querySelector('.training-field__for-text');
 
-let num_of_par = document.querySelector('.num_of_par');
+let numOfPar = document.querySelector('.num_of_par');
 
-render_text(+num_of_par.value);
+render_text(+numOfPar.value);
 
-num_of_par.addEventListener('change', () => {
+numOfPar.addEventListener('change', () => {
     textCont.textContent = '';
-    render_text(+num_of_par.value);
+    render_text(+numOfPar.value);
+})
+
+let startButton = document.querySelector('.start');
+
+startButton.addEventListener('click', () => {
+    startTyping()
 })
